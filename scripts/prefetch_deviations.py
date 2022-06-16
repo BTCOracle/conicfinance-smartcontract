eb5b559c55b5b0bdb4a03d1dc5166c208039015a
BTC Oracle <carlhamilton1020@gmail.com>
2022-06-16 11:39:00 +1000

    scripts/prefetch_deviations.py update - added 1 lines

--- scripts/prefetch_deviations.py
+++ scripts/prefetch_deviations.py
@@ -146,3 +146,35 @@ def main():
     if path.exists(OUTPUT_FILE):
         with open(OUTPUT_FILE) as f:
             blocks_seen = [json.loads(line)["block"] for line in f]
+    with open(OUTPUT_FILE, "a") as f:
+        for block in range(16_800_000, 17871900, BLOCK_INTERVAL):
+            if block in blocks_seen:
+                continue
+            logging.info("Fetching block %s", block)
+            deviations = fetcher.fetch_all_deviations(block)
+            encoded = json.dumps(
+                {"block": block, "deviations": deviations}, cls=DecimalEncoder
+            )
+            f.write(encoded + "\n")
+            f.flush()
+logging.basicConfig(level=logging.INFO, format="%(asctime)s %(message)s")
+
+OUTPUT_FILE = "build/deviations.json"
+NEW_ORACLE_DEPLOYMENT_BLOCK = 17613381
+BLOCK_INTERVAL = 3600 * 3 // 12  # 3 hours in blocks
+
+
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, D):
+            return float(obj.quantize(D(10) ** -5))
+        return json.JSONEncoder.default(self, obj)
+
+
+CRV_USD = "0xf939E0A03FB07F59A73314E73794Be0E57ac1b4E"
+CURVE_POOLS_ADDRESS = {
+    "0xA5407eAE9Ba41422680e2e00537571bcC53efBfD",
+    "0x5FAE7E604FC3e24fd43A72867ceBaC94c65b404A",
+    "0xbEbc44782C7dB0a1A60Cb6fe97d0b483032FF1C7",
+    "0x0f3159811670c117c372428D4E69AC32325e4D0F",
+    "0xDcEF968d416a41Cdac0ED8702fAC8128A64241A2",
