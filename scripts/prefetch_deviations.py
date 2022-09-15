543dfc9f8be6420634213026a85a5cf032c1d714
BTC Oracle <carlhamilton1020@gmail.com>
2022-09-15 13:32:00 +1000

    scripts/prefetch_deviations.py update - added 2 lines

--- scripts/prefetch_deviations.py
+++ scripts/prefetch_deviations.py
@@ -263,3 +263,5 @@ class DataFetcher:
             deviation_bps = (
                 abs(to_expected - to_actual) / max(to_expected, to_actual) * 10_000
             )
+            deviations.append(deviation_bps)
+        return deviations
