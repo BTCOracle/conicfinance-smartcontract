62359e22670f32b3abd2ae1d25b64b38e9887d1d
BTC Oracle <carlhamilton1020@gmail.com>
2023-06-15 12:30:00 +1000

    scripts/prefetch_deviations.py update - added 4 lines

--- scripts/prefetch_deviations.py
+++ scripts/prefetch_deviations.py
@@ -534,3 +534,7 @@ class DataFetcher:
             try:
                 result[pool.address] = self.fetch_pool_deviations(pool, block)
             except Exception as e:
+                logging.error(
+                    "Error fetching pool %s at block %s: %s", pool.address, block, e
+                )
+        return result
