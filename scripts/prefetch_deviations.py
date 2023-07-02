fb55bdf5be801c4aee1d14ba810792306b9f6bac
BTC Oracle <carlhamilton1020@gmail.com>
2023-07-02 14:40:00 +1000

    scripts/prefetch_deviations.py update - added 3 lines

--- scripts/prefetch_deviations.py
+++ scripts/prefetch_deviations.py
@@ -546,3 +546,6 @@ class DataFetcher:
         from_price = prices[0]
         deviations = []
         for i in range(1, len(pool.coins)):
+            to_decimals = pool.coins[i].decimals
+            to_price = prices[i]
+            to_expected_unscaled = D(from_balance * from_price) / to_price
