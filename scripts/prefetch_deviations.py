f53104576de936e30ace76f1f2ef61840bb3ceed
BTC Oracle <carlhamilton1020@gmail.com>
2023-07-27 10:18:00 +1000

    scripts/prefetch_deviations.py update - added 5 lines

--- scripts/prefetch_deviations.py
+++ scripts/prefetch_deviations.py
@@ -569,3 +569,12 @@ class DataFetcher:
     @staticmethod
     def _convert_scale(value: D, from_decimals: int, to_decimals: int) -> D:
         if from_decimals == to_decimals:
+            return value
+        elif from_decimals > to_decimals:
+            return value / D(10 ** (from_decimals - to_decimals))
+        else:
+            return value * D(10 ** (to_decimals - from_decimals))
+
+    def _fetch_prices(self, pool: CurvePool, block: int) -> List[D]:
+        return [self._fetch_price(coin.address, block) for coin in pool.coins]
+
