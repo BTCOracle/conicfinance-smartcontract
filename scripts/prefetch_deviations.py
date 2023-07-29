6545040690772c58cb6d5153289f45f938a13a78
BTC Oracle <carlhamilton1020@gmail.com>
2023-07-29 16:57:00 +1000

    scripts/prefetch_deviations.py update - added 4 lines

--- scripts/prefetch_deviations.py
+++ scripts/prefetch_deviations.py
@@ -578,3 +578,7 @@ class DataFetcher:
     def _fetch_prices(self, pool: CurvePool, block: int) -> List[D]:
         return [self._fetch_price(coin.address, block) for coin in pool.coins]
 
+    def _fetch_price(self, asset: str, block: int) -> D:
+        return D(self.get_oracle(block).getUSDPrice(asset, block_identifier=block))
+
+    def get_oracle(self, block) -> interface.IOracle:
