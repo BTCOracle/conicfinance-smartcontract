7a6a39a44f5ff3d8f95c53e859c040443ef9f7ab
BTC Oracle <carlhamilton1020@gmail.com>
2023-11-29 14:43:00 +1000

    scripts/prefetch_deviations.py update - added 1 lines

--- scripts/prefetch_deviations.py
+++ scripts/prefetch_deviations.py
@@ -685,3 +685,13 @@ class DataFetcher:
             except Exception as e:
                 logging.error(
                     "Error fetching pool %s at block %s: %s", pool.address, block, e
+                )
+        return result
+
+    def fetch_pool_deviations(self, pool: CurvePool, block: int) -> List[D]:
+        prices = self._fetch_prices(pool, block)
+        from_decimals = pool.coins[0].decimals
+        from_balance = 10**from_decimals
+        from_price = prices[0]
+        deviations = []
+        for i in range(1, len(pool.coins)):
