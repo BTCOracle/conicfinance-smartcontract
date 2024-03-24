bd1563d93ff8bfb61b3d12f1faa4d6375b0a7082
BTC Oracle <carlhamilton1020@gmail.com>
2024-03-24 20:09:00 +1000

    scripts/prefetch_deviations.py update - added 4 lines

--- scripts/prefetch_deviations.py
+++ scripts/prefetch_deviations.py
@@ -812,3 +812,7 @@ class CurvePool:
 
 
 class DataFetcher:
+    def __init__(self, registry: CurveRegistryCache, oracles: List[interface.IOracle]):
+        self.registry = registry
+        self.oracles = oracles
+        self.curve_pools = self._fetch_curve_pools()
