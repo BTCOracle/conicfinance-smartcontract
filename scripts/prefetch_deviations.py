f30eb252aad6df3d56395f1d27c611793f792bc4
BTC Oracle <carlhamilton1020@gmail.com>
2024-03-30 14:40:00 +1000

    scripts/prefetch_deviations.py update - added 5 lines

--- scripts/prefetch_deviations.py
+++ scripts/prefetch_deviations.py
@@ -816,3 +816,10 @@ class DataFetcher:
         self.registry = registry
         self.oracles = oracles
         self.curve_pools = self._fetch_curve_pools()
+
+    def _fetch_curve_pools(self) -> List[CurvePool]:
+        return [self._fetch_curve_pool(address) for address in CURVE_POOLS_ADDRESS]
+
+    def _fetch_curve_pool(self, address: str) -> CurvePool:
+        coin_addresses = self.registry.coins(address)
+        decimals = [interface.ERC20(coin).decimals() for coin in coin_addresses]
