dcb093dade9fb2c2aa90c54e4046ceb0171ab3b1
BTC Oracle <carlhamilton1020@gmail.com>
2024-06-19 14:37:00 +1000

    scripts/prefetch_deviations.py update - added 2 lines

--- scripts/prefetch_deviations.py
+++ scripts/prefetch_deviations.py
@@ -884,3 +884,12 @@ class DataFetcher:
     def get_oracle(self, block) -> interface.IOracle:
         if block >= NEW_ORACLE_DEPLOYMENT_BLOCK:
             return self.oracles[1]
+        return self.oracles[0]
+
+
+def main():
+    registry = CurveRegistryCache.at("0x3905A3C1156f67BB55366d7A5a11D1043dcf97c9")
+    new_oracle = interface.IOracle("0x286eF89cD2DA6728FD2cb3e1d1c5766Bcea344b0")
+    old_oracle = interface.IOracle("0x46fa6F8CC35c1F464eA78196080f5Cfd1d76F6E9")
+    fetcher = DataFetcher(registry, [old_oracle, new_oracle])
+
