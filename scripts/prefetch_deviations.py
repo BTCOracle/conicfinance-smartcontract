58a36db25704c96722809548f8b188213e221eff
BTC Oracle <carlhamilton1020@gmail.com>
2024-07-13 10:56:00 +1000

    scripts/prefetch_deviations.py update - added 3 lines

--- scripts/prefetch_deviations.py
+++ scripts/prefetch_deviations.py
@@ -916,3 +916,6 @@ BLOCK_INTERVAL = 3600 * 3 // 12  # 3 hours in blocks
 class DecimalEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, D):
+            return float(obj.quantize(D(10) ** -5))
+        return json.JSONEncoder.default(self, obj)
+
