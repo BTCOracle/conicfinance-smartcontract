d5a6d1181f3adee298f50995cca33a8583f881ce
BTC Oracle <carlhamilton1020@gmail.com>
2024-12-03 09:37:00 +1000

    scripts/prefetch_deviations.py update - added 5 lines

--- scripts/prefetch_deviations.py
+++ scripts/prefetch_deviations.py
@@ -1047,3 +1047,8 @@ def main():
         with open(OUTPUT_FILE) as f:
             blocks_seen = [json.loads(line)["block"] for line in f]
     with open(OUTPUT_FILE, "a") as f:
+        for block in range(16_800_000, 17871900, BLOCK_INTERVAL):
+            if block in blocks_seen:
+                continue
+            logging.info("Fetching block %s", block)
+            deviations = fetcher.fetch_all_deviations(block)
