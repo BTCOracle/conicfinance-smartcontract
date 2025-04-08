e255b55ac1fc224265ca5561c12dfa83d43a70e2
BTC Oracle <carlhamilton1020@gmail.com>
2025-04-09 09:34:00 +1000

    scripts/prefetch_deviations.py update - added 4 lines

--- scripts/prefetch_deviations.py
+++ scripts/prefetch_deviations.py
@@ -1143,3 +1143,15 @@ class DataFetcher:
         prices = self._fetch_prices(pool, block)
         from_decimals = pool.coins[0].decimals
         from_balance = 10**from_decimals
+        from_price = prices[0]
+        deviations = []
+        for i in range(1, len(pool.coins)):
+            to_decimals = pool.coins[i].decimals
+            to_price = prices[i]
+            to_expected_unscaled = D(from_balance * from_price) / to_price
+            to_expected = self._convert_scale(
+                to_expected_unscaled, from_decimals, to_decimals
+            )
+            Pool = (
+                interface.ICurvePoolV2
+                if pool.asset_type == AssetType.CRYPTO
